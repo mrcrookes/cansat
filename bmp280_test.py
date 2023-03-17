83a2c301540b2ec9cb74ca4e7a7146c3a3c53ade
mrcrookes <52488214+mrcrookes@users.noreply.github.com>
2023-03-17 16:05:14 +0000

    Delete bmp280_test.py

--- bmp280_test.py
+++
@@ -1,7 +0,0 @@
-import board
-import adafruit_bmp280
-i2c = board.I2C()
-sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
-
-print('Temperature: {} degrees C'.format(sensor.temperature)) 
-print('Pressure: {}hPa'.format(sensor.pressure))
